fb7829c8889795921319e01fad17883ea9156ebd
Oleksandr2003 <73945156+Oleksandr2003@users.noreply.github.com>
2022-10-17 19:45:29 +0300

    AddMigration

---
+++ Vakansion/Vakansion.Core/Unemployed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vakansion.Core
+{
+    public class Unemployed
+    {
+        [Key]
+        public int ClassId { get; set; }
+        public string? ClassName { get; set; }
+        public int DataId { get; set; }
+        public virtual ICollection<Data>? Datas { get; set; }
+        public int EducationId { get; set; }
+        public virtual ICollection<Education>? Educations { get; set; }
+    }
+}
---
+++ Vakansion/Vakansion.Core/VacancyDbContextExtension.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vakansion.Core
+{
+    public static class VacancyDbContextExtension
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            string ADMIN_ROLE_ID = Guid.NewGuid().ToString();
+            string USER_ROLE_ID = Guid.NewGuid().ToString();
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = ADMIN_ROLE_ID,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = USER_ROLE_ID,
+                    Name = "User",
+                    NormalizedName = "USER"
+                });
+
+            string ADMIN_ID = Guid.NewGuid().ToString();
+            string USER_ID = Guid.NewGuid().ToString();
+
+            var admin = new User
+            {
+                Id = ADMIN_ID,
+                UserName = "admin@vacansyukraine.com",
+                Email = "admin@vacansyukraine.com",
+                EmailConfirmed = true,
+                NormalizedEmail = "admin@vacansyukraine.com".ToUpper(),
+                NormalizedUserName = "admin@vacansyukraine.com".ToUpper()
+            };
+
+            var user = new User
+            {
+                Id = USER_ID,
+                UserName = "user@vacansyukraine.com",
+                Email = "user@vacansyukraine.com",
+                EmailConfirmed = true,
+                NormalizedEmail = "user@vacansyukraine.com".ToUpper(),
+                NormalizedUserName = "user@vacansyukraine.com".ToUpper()
+            };
+
+            PasswordHasher<User> hasher = new PasswordHasher<User>();
+            admin.PasswordHash = hasher.HashPassword(admin, "Admin$pass1");
+            user.PasswordHash = hasher.HashPassword(user, "User$pass1");
+
+            builder.Entity<User>().HasData(admin, user);
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = ADMIN_ROLE_ID,
+                    UserId = ADMIN_ID
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = ADMIN_ROLE_ID,
+                    UserId = USER_ID
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = USER_ROLE_ID,
+                    UserId = USER_ID
+                });
+        }
+    }
+}
