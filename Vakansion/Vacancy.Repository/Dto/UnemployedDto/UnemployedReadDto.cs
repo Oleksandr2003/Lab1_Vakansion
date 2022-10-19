a9067baa354cafbfda56660e5cdd7cc9a0e2983a
Oleksandr2003 <73945156+Oleksandr2003@users.noreply.github.com>
2022-10-19 21:04:18 +0300

    Data Base

--- Vakansion/Vacancy.Repository/Dto/UnemployedDto/UnemployedReadDto.cs
+++ Vakansion/Vacancy.Repository/Dto/UnemployedDto/UnemployedReadDto.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+using Vakansion.Core;
 
-namespace Vakansion.Core
+namespace Vacancy.Repository.Dto.UnemployedDto
 {
-    public class Employer
+    public class UnemployedReadDto
     {
-        [Key]
         public int ClassId { get; set; }
         public string? ClassName { get; set; }
-
+        public int DataId { get; set; }
+        public Data? Datas { get; set; }
+        public int EducationId { get; set; }
+        public Education? Educations { get; set; }
         public virtual ICollection<Vacancys>? Vacancies { get; set; }
     }
 }
---
+++ Vakansion/Vacancy.Repository/Repositories/EducationRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vacancy.Repository.Dto.EducationDto;
+using Vakansion.Core;
+
+namespace Vacancy.Repository.Repositories
+{
+    public class EducationRepository
+    {
+        private readonly VacancyContext _ctx;
+
+        public EducationRepository(VacancyContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<IEnumerable<EducationReadDto>> GetBodyTypesAsync()
+        {
+
+            var educationDto = _ctx.Educations.
+            Select(x => new EducationReadDto
+            {
+                ClassId = x.ClassId,
+                ClassName = x.ClassName,
+                Unemployed = x.Unemployeds,
+                Vacancies = x.Vacancies,
+            }).ToList();
+
+            return educationDto;
+        }
+    }
+}
